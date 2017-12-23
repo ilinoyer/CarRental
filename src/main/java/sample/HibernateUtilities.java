a4d2088d9bda271e4738eeba4182be09c9e161d3
ilinoyer <tmjaskowiec@gmail.com>
2017-12-23 22:28:21 +0100

    Project Commit

---
+++ src/main/java/sample/HibernateUtilities.java
@@ -0,0 +1,46 @@
+package sample;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtilities {
+    private static SessionFactory sessionFactory;
+    private static StandardServiceRegistry registry;
+
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                // Create registry
+                registry = new StandardServiceRegistryBuilder()
+                        .configure("hibernate.cfg.xml")
+                        .build();
+
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
+
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
+
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.hibernate.Session;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Car Rental");
+        final MainController maincontroller = loader.getController();
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(600);
+        primaryStage.show();
+
+
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
