9160ac1cedea189a5090b4a9d8b3cb2c6c7fd56a
ilinoyer <tmjaskowiec@gmail.com>
2017-12-29 01:31:22 +0100

    .

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.controllers.MainController;
 
 public class Main extends Application {
 
